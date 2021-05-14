ec1931ad69475784985575e2b8a64bb15167ac32
asqwklop12 <asqwklop12@gmail.com>
2021-05-15 03:23:29 +0900

    init

---
+++ src/main/java/com/example/quartzp/QuartzPApplication.java
@@ -0,0 +1,13 @@
+package com.example.quartzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzPApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuartzPApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/quartzp/QuartzPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzPApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
